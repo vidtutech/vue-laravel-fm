52d0f00304315e83ad5385ac01bb1bd9f9b2e55e
Brian Sam Thomas <thebrainchild95@gmail.com>
2024-03-09 16:54:50 -0500

    refactor: refactored main.js to match vidtu's logic

--- src/main.js
+++ src/main.js
@@ -5,23 +5,26 @@ import fm from './store';
 import FMApp from './FileManager.vue';
 import ClickOutside from './directives/ClickOutside';
 
-export const loadFm = async (path = 'room') => {
+const loadFm = async (path = 'documents') => {
     const app = createApp(FMApp);
 
-    const storeModules = Object.assign({}, Object.freeze({ strict: true, modules: { fm } }));
+    const storeModules = Object.freeze({ strict: true, modules: { fm } });
 
     const store = createStore(storeModules);
 
     app.use(store).directive('click-outside', ClickOutside);
 
     window['fm'] = app;
+
     window['sharedFm'] = app;
 
-    if (path === 'shared') {
-        window['sharedFm'].mount('#shared-fm');
+    if (path === 'shared-documents') {
+        window['sharedFm'].mount('#shared-documents-fm');
     } else {
-        window['fm'].mount('#fm');
+        window['fm'].mount('#documents-fm');
     }
 };
 
 window.loadFm = loadFm;
+
+export default loadFm;
